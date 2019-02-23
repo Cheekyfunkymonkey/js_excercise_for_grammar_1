db25d5db26fe8cc72c5b3eb3e75ac51ed02884e8
Cheekyfunkymonkey <shigechu@gmail.com>
2019-02-24 08:52:42 +1300

    do exersice

---
+++ main.js
@@ -0,0 +1,24 @@
+const lastName = "山田";
+const firstName = "花子";
+const fullName = lastName + " " + firstName;
+const myAge = 20;
+const friendAge = 24;
+const totalAge = myAge + friendAge;
+const numberOfDays = myAge * 2 * 365;
+const numberOfChildren = 3;
+const numberOfCakes = 10;
+const numberOfCakesByChild = numberOfCakes / numberOfChildren;
+const numberOfRestOfCakes = numberOfCakes % numberOfChildren;
+
+console.log(
+  lastName,
+  firstName,
+  fullName,
+  myAge,
+  friendAge,
+  totalAge,
+  numberOfDays,
+  numberOfCakes,
+  numberOfCakesByChild,
+  numberOfRestOfCakes
+);
